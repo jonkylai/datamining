ccf26b44dcb04efe1413cb42749b3242679b0d3d
unknown <jonkylai@gmail.com>
2020-09-07 12:08:09 -0700

    Add persistence in mining data

--- DetectSteamBot/SteamMine.py
+++ DetectSteamBot/SteamMine.py
@@ -1,3 +1,5 @@
+import pickle
+import os
 import time
 import requests
 
@@ -6,6 +8,8 @@ from bs4 import BeautifulSoup
 
 HUMAN_FILE = 'truthlist.txt'
 BOT_FILE = 'blacklist.txt'
+HUMAN_PICKLE = 'steam_human.p'
+BOT_PICKLE = 'steam_bot.p'
 
 
 def get_accounts(file_in: str) -> list:
@@ -19,6 +23,10 @@ def get_accounts(file_in: str) -> list:
                 first_char = line[0]
             except IndexError:
                 first_char = '#'
+
+            # Stop reading once ignore list is reached
+            if '# Ignore list' in line:
+                break
 
             # Ignore comments and non account format
             if first_char == 'h':
@@ -77,6 +85,33 @@ def get_avatar(soup_in: BeautifulSoup) -> str:
     return string_out
 
 
+def get_hour(soup_in: BeautifulSoup) -> list:
+    text_list = soup_in.find_all('div', {'class': 'game_info_details'})
+
+    hour_list = list()
+    for text in text_list:
+        try:
+            # Where the 3rd item should be hrs on record
+            hours = text.prettify().split()[2]
+            # Where numbers greater than 3 digits have commas
+            hour_list.append( float( hours.replace(',','') ) )
+        except:
+            print('ERROR: Cannot get hours played from account')
+            exit()
+
+    return hour_list
+
+
+def get_comment(soup_in: BeautifulSoup, class_in: str) -> list:
+    """ Get comment date or text from account """
+    text_list = soup_in.find_all(class_=class_in)
+
+    comment_list = list()
+    for text in text_list:
+        comment_list.append(text.text.strip())
+    return comment_list
+
+
 def get_text_between(string_in: str, begin_in: str, end_in: str) -> str:
     """ Parses text by stripping text before and after
         Error checking included """
@@ -89,47 +124,75 @@ def get_text_between(string_in: str, begin_in: str, end_in: str) -> str:
         print('       %s' % string_in)
 
 
+def in_dict(url_in: str, dictionary_list: list) -> bool:
+    """ Check if string already exists in dictionary with url key """
+    for dictionary in dictionary_list:
+        if url_in == dictionary['url']:
+            return True
+    return False
+
+
 def main():
-    """ Read list of Steam accounts and stores the data """
-    human_list = get_accounts(HUMAN_FILE)
-    bot_list = get_accounts(BOT_FILE)
+    """ Do same tasks for both human and bot list """
+    steam_list = [ [HUMAN_PICKLE, HUMAN_FILE],
+                   [BOT_PICKLE,   BOT_FILE] ]
 
-    for url in human_list:
+    """ Load previously loaded data if it exists """
+    for fetch_list in steam_list:
+        fetch_pickle = fetch_list[0]
+        fetch_file = fetch_list[1]
 
-        # Benchmark requests.get() speed
-        time_page = time.time()
-        page = requests.get(url)
-        print('--- %0.4f sec --- %s' % (time.time() - time_page, page.url))
+        if os.path.exists(fetch_pickle):
+            steam_data = pickle.load(open(fetch_pickle, 'rb'))
+        else:
+            steam_data = list()
 
-        # Benchmark soup speed
-        time_page = time.time()
-        page_soup = BeautifulSoup(page.content, 'html.parser')
+        """ Read list of Steam accounts and stores the data """
+        url_list = get_accounts(fetch_file)
 
-        avatar = get_avatar(page_soup)
-        level = get_count(page_soup, 'persona_level')
-        summary = get_summary(page_soup, 'profile_summary')
-        count_games = get_sidebar(page_soup, 'Games')
-        count_inventory = get_sidebar(page_soup, 'Inventory')
-        count_screenshots = get_sidebar(page_soup, 'Screenshots')
-        count_videos = get_sidebar(page_soup, 'Videos')
-        count_workshop = get_sidebar(page_soup, 'Workshop Items')
-        count_reviews = get_sidebar(page_soup, 'Reviews')
-        count_guides = get_sidebar(page_soup, 'Guides')
-        count_artwork = get_sidebar(page_soup, 'Artwork')
-        count_groups = get_count(page_soup, 'profile_group_links')
-        count_friends = get_count(page_soup, 'profile_friend_links')
+        for url in url_list:
+            if in_dict(url, steam_data):
+                continue
 
+            fetch_dict = dict()
 
-        # Get account level
-        print('--- %0.4f sec --- soup time' % (time.time() - time_page))
-        print(level)
-        print(avatar)
-        print(summary)
-        print(count_games, count_inventory, count_screenshots, count_videos, count_workshop, count_reviews, count_guides, count_artwork)
-        print(count_groups, count_friends)
+            # Benchmark requests.get() speed
+            time_page = time.time()
+            page = requests.get(url)
+            print('--- %0.4f sec --- %s' % (time.time() - time_page, page.url))
 
-        exit()
+            time_page = time.time()
+            page_soup = BeautifulSoup(page.content, 'html.parser')
 
+            fetch_dict['url'] = url
+
+            fetch_dict['avatar'] = get_avatar(page_soup)
+            fetch_dict['level'] = get_count(page_soup, 'persona_level')
+
+            fetch_dict['summary'] = get_summary(page_soup, 'profile_summary')
+
+            fetch_dict['games'] = get_sidebar(page_soup, 'Games')
+            fetch_dict['inventory'] = get_sidebar(page_soup, 'Inventory')
+            fetch_dict['screenshots'] = get_sidebar(page_soup, 'Screenshots')
+            fetch_dict['videos'] = get_sidebar(page_soup, 'Videos')
+            fetch_dict['workshop'] = get_sidebar(page_soup, 'Workshop Items')
+            fetch_dict['reviews'] = get_sidebar(page_soup, 'Reviews')
+            fetch_dict['guides'] = get_sidebar(page_soup, 'Guides')
+            fetch_dict['artwork'] = get_sidebar(page_soup, 'Artwork')
+
+            fetch_dict['groups'] = get_count(page_soup, 'profile_group_links')
+            fetch_dict['friends'] = get_count(page_soup, 'profile_friend_links')
+
+            fetch_dict['hour_list'] = get_hour(page_soup)
+
+            fetch_dict['stamp_list'] = get_comment(page_soup, 'commentthread_comment_timestamp')
+            fetch_dict['comment_list'] = get_comment(page_soup, 'commentthread_comment_text')
+
+            steam_data.append(fetch_dict)
+
+        pickle.dump(steam_data, open(fetch_pickle, 'wb'))
+
+        print( '%s has %i entries' % (fetch_pickle, len(steam_data)) )
 
 if __name__ == "__main__":
     main()
